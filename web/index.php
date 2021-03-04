078da3fa2bf07c68da22e655b2bcbb5dde536419
Valentin Dell'angela <valentin.dellangela@bigcommerce.com>
2021-03-04 15:42:18 +1100

    updating the webhooks method

--- web/index.php
+++ web/index.php
@@ -21,45 +21,65 @@ $app->post('/webhooks/cart_updated', function() use($app) {
 
 $app->post('/webhooks', function(Request $request) use($app) {
     $client = new GuzzleHttp\Client();
-    $client->request('POST', 'https://webhook.site/3126fcc2-fe6f-4518-bd00-8c5c8e09ad36', [
-        GuzzleHttp\RequestOptions::JSON => [$request->get('test')]]
+    $storeHash = 'tb0i4pdxam';
+    $headers = array('X-Auth-Client' => 'bvq6cndv138suc8ttz4go3bfdqvwtfj', 'X-Auth-Token' => 'kw28zub9tnz3jjfmsf5gpf6lztjdp2s');
+    $body = json_decode($request->getContent(), true);
+
+    if (empty($body) || !$body['scope'] === 'store/cart/converted') {
+        return 'wrong scope';
+    }
+
+    $orderId = $body['data']['orderId'];
+
+    // Retrieve Order's currency
+    $response = $client->request(
+'POST',
+    'https://api.bigcommerce.com/stores/'.$storeHash.'/v2/orders/'.$orderId,
+        ['headers' => $headers]
     );
+    $body = json_decode($response->getBody());
+    $currency = $body['currency_code'];
 
-    return '';
+    // Retrieve Orders' items info
+    $response = $client->request(
+        'POST',
+        'https://api.bigcommerce.com/stores/'.$storeHash.'/v2/orders/'.$orderId.'/products',
+        ['headers' => $headers]
+    );
+    $body = json_decode($response->getBody());
+    $items = [];
 
-    // if (!$request->get('scope') === 'store/cart/converted') {
-    //     return;
-    // }
-    //
-    // $response = $client->request('POST', 'https://api-staging.cooler.dev/v1/footprint/products', [
-    //     GuzzleHttp\RequestOptions::JSON => [
-    //         'currency' => 'USD',
-    //         'items' => [
-    //             [
-    //                 'product_id' => 'B2-R',
-    //                 'quantity' => 1,
-    //                 'price' => 599
-    //             ]
-    //         ]
-    //     ]
-    // ]);
-    //
-    // if ($response->getStatusCode() === 200) {
-    //     $body = json_decode($response->getBody());
-    //     $neutralisationId = $body['id'];
-    //
-    //     $response = $client->request('POST', 'https://api-staging.cooler.dev/v1/footprint/products', [
-    //         GuzzleHttp\RequestOptions::JSON => [
-    //             'transactions' => [$neutralisationId]
-    //         ]
-    //     ]);
-    //
-    //     if ($response->getStatusCode() === 200) {
-    //         return 'success!';
-    //     }
-    // }
-    //
-    // return 'failed :(';
+    foreach ($body as $product) {
+        $items[] = [
+            'product_id' => $product['product_id'],
+            'quantity' => $product['quantity'],
+            'price' => $product['total_inc_tax']
+        ];
+    }
+
+    $response = $client->request('POST', 'https://api-staging.cooler.dev/v1/footprint/products', [
+        GuzzleHttp\RequestOptions::JSON => [
+            'currency' => $currency,
+            'items' => $items
+        ]
+    ]);
+
+    if ($response->getStatusCode() === 200) {
+        $body = json_decode($response->getBody());
+        $neutralisationId = $body['id'];
+
+        $response = $client->request('POST', 'https://api-staging.cooler.dev/v1/footprint/products', [
+            GuzzleHttp\RequestOptions::JSON => [
+                'transactions' => [$neutralisationId]
+            ]
+        ]);
+
+        if ($response->getStatusCode() === 200) {
+            return 'success!';
+        }
+    }
+
+    return 'failed :(';
 });
 
 
